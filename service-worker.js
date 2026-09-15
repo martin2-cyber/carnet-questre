1c4e88eb587ac0fdd92cfaab7ace56fc1f07cfa7
martin2-cyber <martin1307martin@gmail.com>
2026-09-15 12:58:16 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "carnet-equestre-v1";
+const FICHIERS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FICHIERS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copie = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copie));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
